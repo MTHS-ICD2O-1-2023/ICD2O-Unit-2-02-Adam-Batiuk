3c673ac1cfe78795ca78c42d0e729b0880b8d8aa
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-03-08 19:21:37 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Adam Batiuk All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adam Batiuk
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  document.getElementById("area").innerHTML = "Area is: " + (5 * 3) + " cm²"
+  document.getElementById("perimeter").innerHTML = "Perimeter is: " + (2 * (5 + 3)) + " cm"
+}
